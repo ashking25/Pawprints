cd57a99d3b5f06f752c14139b4a9fe19c8945057
Ubuntu <ashking25@gmail.com>
2019-01-31 20:53:47 +0000

    updated model

--- transfer_learning_tf.py
+++ transfer_learning_tf.py
@@ -13,17 +13,17 @@ from keras.preprocessing import image
 from keras.models import Model
 from keras.models import load_model, Sequential
 from keras.layers import Dense, GlobalAveragePooling2D
-from keras.layers import Input, Concatenate, Lambda
+from keras.layers import Input, Concatenate, Lambda, BatchNormalization
 from keras import backend as K
 import keras
 from sklearn.utils import class_weight
 from keras.callbacks import TensorBoard
 from skimage import exposure
 import sys
-import tensorflow_hub as hub
-import tensorflow as tf
-hub_to_use="https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/1"
-
+#import tensorflow_hub as hub
+#import tensorflow as tf
+#hub_to_use="https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/1"
+from imblearn.over_sampling import RandomOverSampler 
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -37,7 +37,7 @@ elif cnn_model == 'VGG':
 elif cnn_model == 'ResNet':
     from keras.applications.resnet50 import preprocess_input
 
-def imageLoader(files, batch_size, y_train, df_name):
+def imageLoader(files, base_model, batch_size, y_train, df_name):
 
     L = len(files)
 
@@ -49,7 +49,8 @@ def imageLoader(files, batch_size, y_train, df_name):
 
         while batch_start < L:
             limit = min(batch_end, L)
-            X = load_images_from_list(files[batch_start:limit], xpixels=224, ypixels=224)
+            X = load_images_from_list(files[batch_start:limit], base_model, xpixels=500, ypixels=500)
+            #X = load_images_npy(files[batch_start:limit])
             X = np.reshape(np.array(X), (np.shape(X)[0], np.shape(X)[1],
                                          np.shape(X)[2], np.shape(X)[3]))
             X_metadata = load_metadata(files[batch_start:limit])
@@ -64,29 +65,35 @@ def load_images_npy(files):
     # just flip and rotate at random
     images = []
     for f in files:
-        x = np.load('../images_footprints/'+str(int(f))+'.npy')
-        x = np.rot90(x,k=np.random.choice(range(4)))
-        if np.random.choice(range(2)) == 1:
-            x = np.transpose(x, axes=[1,0,2])
-        x = np.expand_dims(x, axis=0)
-        images.append(x[0])
+        x = np.load('../'+f[:-4]+'_incept.npy')#/images_footprints/'+str(int(f))+'.npy')
+        #x = np.rot90(x,k=np.random.choice(range(4)))
+        #if np.random.choice(range(2)) == 1:
+        #    x = np.transpose(x, axes=[1,0,2])
+        #x = np.expand_dims(x, axis=0)
+        images.append(x)
     return images
 
-def load_images_from_list(files, xpixels=299, ypixels=299):
+def load_images_from_list(files, base_model, xpixels=299, ypixels=299):
 
     images = []
+    if cnn_model == 'ResNet':
+        xpixels = 224
+        ypixels = 224
     for f in files:
         #load images
         img_path = '../'+f
-        img = image.load_img(img_path, target_size=(xpixels, ypixels))
+        img = image.load_img(img_path)#, target_size=(xpixels, ypixels))
         x = image.img_to_array(img)
         x[np.isnan(x)]=0
-        if 1 == 10:
+        #if 1 == 1:
             # don't need to smooth or flip right now
-            x = exposure.equalize_adapthist(x/np.max(x), clip_limit=0.03)*256
-            x = np.rot90(x,k=np.random.choice(range(4)))
-            if np.random.choice(range(2)) == 1:
-                x = np.transpose(x, axes=[1,0,2])
+        x = x/np.max(x)
+        x = exposure.adjust_gamma(x, gamma=1./2.2, gain=1)*256
+        
+            #x = exposure.equalize_adapthist(x/np.max(x), clip_limit=0.03)*256
+            #x = np.rot90(x,k=np.random.choice(range(4)))
+            #if np.random.choice(range(2)) == 1:
+            #    x = np.transpose(x, axes=[1,0,2])
 
         #Reshape
         if x.shape[0] < xpixels or x.shape[1] < ypixels:
@@ -106,13 +113,16 @@ def load_images_from_list(files, xpixels=299, ypixels=299):
         x = np.expand_dims(x, axis=0)
 
         x = preprocess_input(x)
+        #predict = base_model.predict(x)
         images.append(x[0])
-    return images
+    images_predict = base_model.predict(np.array(images))
+    
+    return images_predict
 
 def load_metadata(files):
     """ Load information about size, location, and day of the year """
     results = []
-    df= pd.read_csv('df_measurements_50.csv',index_col=0 )
+    df= pd.read_csv('../df_measurements_50.csv',index_col=0 )
     for f in files:
         if 'NonPawprints' in f:
             size = np.random.uniform(low=0.1, high=12)
@@ -123,8 +133,9 @@ def load_metadata(files):
         else:
             f = f.split('/')[-1]
             f = np.int(f[:-4])
-            size = np.random.uniform(low=df['min_track'].loc[f], high=df['max_track'].loc[f])#/12. # normalize to 1
 
+            size = np.random.normal(loc=(df['min_track'].loc[f]+df['max_track'].loc[f])/2., 
+                                    scale=(df['max_track'].loc[f]-df['min_track'].loc[f])/2./np.sqrt(2.))#/12. # normalize to 1
             day_sin = np.sin(df['DayofYear'].loc[f]/365.*np.pi*2.)+1
             day_cos = np.cos(df['DayofYear'].loc[f]/365.*np.pi*2.)+1
 
@@ -134,46 +145,54 @@ def load_metadata(files):
             if np.isnan(np.sum(lat)) or np.isnan(np.sum(long)):
                 lat = 0
                 long = 0
+            if np.random.uniform(0, 1) < .10:
+                lat = 0
+                long = 0
         results.append([size, day_sin, day_cos, lat, long])
     return results
 
 def my_model(cnn_model, class_weights, target_width=224):
     # create the base pre-trained model
     if cnn_model == 'Inception':
-        base_model = InceptionV3(weights='imagenet', include_top=False)
+        x_input = Input(batch_shape=(None, None, None, 2048))
     elif cnn_model == 'VGG':
-        base_model = VGG16(weights='imagenet', include_top=False)
+        x_input = Input(batch_shape=(None, None, None, 512))
     elif cnn_model == 'ResNet':
-        base_model = ResNet50(weights='imagenet', include_top=True)
-
+        x_input = Input(batch_shape=(None, None, None, 2048))
     #medtadata
     metadata_input = Input(batch_shape=(None,5))
 
     # add a global spatial average pooling layer
-    x = base_model.output
-    if cnn_model != 'ResNet':
-        x = GlobalAveragePooling2D()(x)
-        #  add some fully-connected layers
+    #x_input = Input(batch_shape=(None, None, None, 2048))
+    #x = base_model.output
+    print('shape',np.shape(X_validate_generated[0]))
+    
+    #if cnn_model != 'ResNet':
+    x = GlobalAveragePooling2D()(x_input)
+    #  add some fully-connected layers
     a = Concatenate()([x, metadata_input])
 
     x = Dense(1024, activation='relu')(a)
+    #x = BatchNormalization()(x)
     x = Dense(512, activation='relu')(x)
-
+    #x = BatchNormalization()(x)
     # and a logistic layer -- let's say we have 25 classes
     predictions = Dense(len(class_weights), activation='softmax')(x)
 
     # this is the model we will train
-    model = Model(inputs=[base_model.input, metadata_input], outputs=predictions)
+    model = Model(inputs=[x_input, metadata_input], outputs=predictions)
 
     # first: train only the top layers (which were randomly initialized)
     # i.e. freeze all convolutional InceptionV3 layers
-    for layer in base_model.layers:
-        layer.trainable = False
+    #for layer in base_model.layers:
+    #    layer.trainable = False
 
     # compile the model (should be done *after* setting layers to non-trainable)
     adam = keras.optimizers.Adam(lr=0.003, beta_1=0.9, beta_2=0.999, epsilon=None,
                                  decay=0.0, amsgrad=False)
-    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])#,
+#    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])#,
+
+    model.compile(optimizer=adam, loss='kullback_leibler_divergence', metrics=['accuracy'])
     return model
 
 def load_data(filename):
@@ -187,6 +206,10 @@ def load_data(filename):
     X_train, X_test, y_train, y_test = train_test_split(names,y, test_size=0.20, random_state=42)
     X_validate, X_test, y_validate, y_test = train_test_split(X_test, y_test, test_size=0.50, random_state=42)
 
+    print('here')
+#    ros = RandomOverSampler(random_state=42, sampling_strategy='not majority')
+#    X_train, y_train= ros.fit_sample(np.reshape(X_train, (len(X_train), 1)), y_train)
+#    X_train = X_train[:,0]
     df= pd.read_csv(filename, index_col=0)
 
     # integer encode
@@ -212,51 +235,69 @@ def load_data(filename):
 if __name__ == '__main__':
     # Load Data
 
-    df_name = 'df_measurements_50.csv'
+    df_name = '../df_measurements_50.csv'
     X_train, X_validate, onehot_encoded, onehot_encoded_validate, class_weights = \
         load_data(df_name)
-
-    validate_generator = imageLoader(X_validate, 500, onehot_encoded_validate, df_name)
+    # Load pretrained models
+    # create the base pre-trained model
+    if cnn_model == 'Inception':
+        base_model = InceptionV3(weights='imagenet', include_top=False)
+    elif cnn_model == 'VGG':
+        base_model = VGG16(weights='imagenet', include_top=False)
+    elif cnn_model == 'ResNet':
+        base_model = ResNet50(weights='imagenet', include_top=False)    
+    print('here')
+    validate_generator = imageLoader(X_validate, base_model, 8, onehot_encoded_validate, df_name)
     X_validate_generated, onehot_encoded_validate_generated = next(validate_generator)
-    train_generator = imageLoader(X_train, 4, onehot_encoded, df_name)
-
+    for i in range(50):
+        print('validate', i)
+        # add more validation data
+        X_validate_generated_extra, onehot_encoded_validate_generated_extra = next(validate_generator)
+        X_validate_generated[0]= np.append(X_validate_generated[0], X_validate_generated_extra[0], axis=0)
+        X_validate_generated[1]= np.append(X_validate_generated[1], X_validate_generated_extra[1], axis=0)
+        onehot_encoded_validate_generated= np.append(onehot_encoded_validate_generated, 
+                                                 onehot_encoded_validate_generated_extra, axis=0)
+    train_generator = imageLoader(X_train, base_model, 8, onehot_encoded, df_name)
+    print('built generators')
     # Load Model
-    if os.path.isfile('model_'+cnn_model+'.h5'):
-        model = load_model('model_'+cnn_model+'.h5')
+    if os.path.isfile('../model_'+cnn_model+'.h5'):
+        model = load_model('../model_'+cnn_model+'.h5')
         # compile the model (should be done *after* setting layers to non-trainable)
         adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None,
                                      decay=0.0, amsgrad=False)
-        model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])#,
+#        model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])#,
+        model.compile(optimizer=adam, loss='kullback_leibler_divergence', metrics=['accuracy'])
     else:
-        final_model = my_model(cnn_model, class_weights)
+        model = my_model(cnn_model, class_weights)
         #print(final_model.summary())
         # train the model on the new data for a few epoch
-        final_model.fit_generator(train_generator, steps_per_epoch=100, epochs=10, \
+        model.fit_generator(train_generator, steps_per_epoch=100, epochs=5, \
             verbose=1, class_weight = class_weights,\
             validation_data=(X_validate_generated, onehot_encoded_validate_generated))
 
         #Save weights
-        model.save('model_'+cnn_model+'.h5')
+        model.save('../model_'+cnn_model+'.h5')
 
     # Unfreeze a number of CNN layers
     for layer in model.layers:
        layer.trainable = True
 
     # compile the model (should be done *after* setting layers to non-trainable)
-    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None,
+    adam = keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None,
                                  decay=0.01, amsgrad=False)
-    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-
+    #model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=adam, loss='kullback_leibler_divergence', metrics=['accuracy'])
 
     #callbacks
-    callbacks = keras.callbacks.ModelCheckpoint('../', monitor='val_loss',
+    callbacks = keras.callbacks.ModelCheckpoint('../model_'+cnn_model+'.h5', monitor='val_loss',
         verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=5)
 
 
     # train the model on the new data for a few epoch
-    model.fit_generator(train_generator, steps_per_epoch=1000, epochs=100, \
-        verbose=1, class_weight = class_weights,\
-        validation_data=(X_validate_generated, onehot_encoded_validate_generated))
+    model.fit_generator(train_generator, steps_per_epoch=100, epochs=100, \
+        verbose=2, class_weight = class_weights,\
+        validation_data=(X_validate_generated, onehot_encoded_validate_generated),\
+        callbacks=[callbacks])
 
     #Save weights
-    model.save('model_'+cnn_model+'.h5')
+    #model.save('model_'+cnn_model+'.h5')
